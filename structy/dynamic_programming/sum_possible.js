f9d6e9b87c13f551a17570253d85e5b230af4eaf
cfthorne <cfthorne83@gmail.com>
2022-01-03 15:58:56 -0500

    add buildtree

--- structy/dynamic_programming/sum_possible.js
+++ structy/dynamic_programming/sum_possible.js
@@ -1,36 +1,75 @@
-class Node{
-    constructor(val, prev=null){
+// class Node{
+//     constructor(val, prev=null){
+//         this.val = val;
+//         this.prev = prev;
+//         this.sum = this.prev ? this.prev.sum + this.val : this.val;
+//     }
+// }
+
+// const sumPossible = (amount, numbers) => {
+
+//     for (let n of numbers){
+//         if (buildTree(n, amount, numbers)) return true;
+//     }
+//     return false;
+// };
+
+// const buildTree = (root, amount, numbers) => {
+//     let stack = [new Node(root)];
+
+//     while (stack.length){
+//         let current = stack.pop();
+
+//         for (let num of numbers){
+//             if (num + current.sum === amount){
+//                 return true;
+//             } else if ((num  + current.sum) < amount){
+//                 let newNode = new Node(num, current)
+//                 stack.push(newNode);
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+///////////////////////////////////////////////////////////
+
+class Node {
+    constructor(val){
         this.val = val;
-        this.prev = prev;
-        this.sum = this.prev ? this.prev.sum + this.val : this.val;
     }
 }
+
 
 const sumPossible = (amount, numbers) => {
 
-    for (let n of numbers){
-        if (buildTree(n, amount, numbers)) return true;
+    for (let num of numbers){
+        num.buildTree(num, numbers, amount)
     }
+};
+
+const buildTree = (root, numbers, amount) => {
+    if ((amount - root) === 0) return true;
+    if ((amount - root) > 0) return false;
+
+    let newAmount = amount - root;
+
+    for (let num of numbers) {
+        if (buildTree(num, numbers, newAmount) === true) return true;
+    }
+
     return false;
 };
 
-const buildTree = (root, amount, numbers) => {
-    let stack = [new Node(root)];
 
-    while (stack.length){
-        let current = stack.pop();
 
-        for (let num of numbers){
-            if (num + current.sum === amount){
-                return true;
-            } else if ((num  + current.sum) < amount){
-                let newNode = new Node(num, current)
-                stack.push(newNode);
-            }
-        }
-    }
-    return false;
-}
+
+
+
+
+
+
+
 
 // console.log(buildTree(12, 8, [5, 12, 4] ))
 console.log(sumPossible(8, [5, 12, 4])); // -> true, 4 + 4
