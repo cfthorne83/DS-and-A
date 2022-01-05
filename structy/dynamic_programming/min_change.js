5f195b0a37dedf34497b5b3d7ed1fb838f8ff1fc
cfthorne <cfthorne83@gmail.com>
2022-01-05 10:54:04 -0500

    add minChange

---
+++ structy/dynamic_programming/min_change.js
@@ -0,0 +1,12 @@
+// Write a function minChange that takes in an amount and an array of coins.The 
+// function should return the minimum number of coins required to create the 
+// amount.You may use each coin as many times as necessary.
+
+// If it is not possible to create the amount, then return -1.
+
+const minChange = (amount, coins) => {
+    if ( amount === 0 ) return true;
+    
+};
+
+// minChange(8, [1, 5, 4, 12]); // -> 2, because 4+4 is the minimum coins possible
