3aa95dedd486343292bbf9c726c9c7a767d8e2bb
cfthorne <cfthorne83@gmail.com>
2022-01-09 14:00:26 -0500

    debug

--- structy/dynamic_programming/count_paths.js
+++ structy/dynamic_programming/count_paths.js
@@ -5,17 +5,18 @@
 // the bottom-right corner.
 
 const countPaths = (grid, row=0, col=0, count=0) => {
-    if (row === grid.length - 1 && col === grid[0].length - 1) return 1;
-    if (grid[row][col] === "W") return 0;
+    // if (row === grid.length - 1 && col === grid[0].length - 1) return 1;
+    // if (grid[row][col] === "W") return 0;
 
-    let right = countPaths(grid, row + 1, col, count);
-    let down = countPaths(grid, row, col + 1, count);
+    // let right = countPaths(grid, row + 1, col, count);
+    // let down = countPaths(grid, row, col + 1, count);
 
-    return  right + down;
+    // return  right + down;
+    return grid[row][col] === "W";
 };
 
-// const grid = [
-//   ["O", "O"],
-//   ["O", "O"],
-// ];
-// countPaths(grid); // -> 2
+const grid = [
+  ["O", "O"],
+  ["O", "O"],
+];
+console.log(countPaths(grid)); // -> 2
