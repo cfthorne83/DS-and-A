651d18b81d51e186c8c06890801289c80ff6fa4e
cfthorne <cfthorne83@gmail.com>
2022-01-09 13:32:43 -0500

    add instructions/test

--- structy/dynamic_programming/count_paths.js
+++ structy/dynamic_programming/count_paths.js
@@ -1,3 +1,15 @@
+// Write a function, countPaths, that takes in a grid as an argument. In the 
+// grid, 'X' represents walls and 'O' represents open spaces. You may only move 
+// down or to the right and cannot pass through walls. The function should return 
+// the number of ways possible to travel from the top-left corner of the grid to 
+// the bottom-right corner.
+
 const countPaths = (grid) => {
 
 };
+
+// const grid = [
+//   ["O", "O"],
+//   ["O", "O"],
+// ];
+// countPaths(grid); // -> 2
