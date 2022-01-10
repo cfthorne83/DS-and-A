6611a80c5836f282272585b306905ed3cc38042d
cfthorne <cfthorne83@gmail.com>
2022-01-10 11:15:24 -0500

    add base cases countPaths

--- structy/dynamic_programming/count_paths.js
+++ structy/dynamic_programming/count_paths.js
@@ -5,14 +5,15 @@
 // the bottom-right corner.
 
 const countPaths = (grid, row=0, col=0, count=0) => {
-    // if (row === grid.length - 1 && col === grid[0].length - 1) return 1;
-    // if (grid[row][col] === "W") return 0;
+    if ( row >= grid.length) return 0;
+    if ( col >= grid[0].length) return 0;
+    if (row === grid.length - 1 && col === grid[0].length - 1) return 1;
+    if (grid[row][col] === "X") return 0;
 
-    // let right = countPaths(grid, row + 1, col, count);
-    // let down = countPaths(grid, row, col + 1, count);
+    let right = countPaths(grid, row + 1, col, count);
+    let down = countPaths(grid, row, col + 1, count);
 
-    // return  right + down;
-    return grid[row][col] === "W";
+    return  right + down;
 };
 
 const grid = [
