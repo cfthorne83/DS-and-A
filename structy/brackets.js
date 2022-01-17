822dc066b28d57e3fb9ef01ec29df282c7cb72a7
cfthorne <cfthorne83@gmail.com>
2022-01-17 15:50:10 -0500

    add brackets

---
+++ structy/brackets.js
@@ -0,0 +1,20 @@
+function SearchingChallenge(str) { 
+  let stack = [];
+
+  for (let char of str){
+    if (char === "("){
+      stack.push("(")
+    } else if (char === ")"){
+      if (!stack.length) return 0;
+      stack.pop();
+    }
+  }
+  
+  if (stack.length === 0){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+let str = 
+SearchingChallenge()
