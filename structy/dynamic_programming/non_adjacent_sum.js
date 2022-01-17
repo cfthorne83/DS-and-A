916913a5f471b939030826ba0bb384a06fdc424a
cfthorne <cfthorne83@gmail.com>
2022-01-17 15:10:51 -0500

    make recursive

--- structy/dynamic_programming/non_adjacent_sum.js
+++ structy/dynamic_programming/non_adjacent_sum.js
@@ -3,18 +3,19 @@
 // in the array. There is no limit on how many elements can be taken into the sum 
 // as long as they are not adjacent.
 
-const nonAdjacentSum = (nums) => {
-    if (nums.length === 0) return 0;
+const nonAdjacentSum = (nums, i=0) => {
+    if (i >= nums.length) return 0;
 
-    let exclude = nonAdjacentSum(nums.slice(1));
-    let include = nums[0] + nonAdjacentSum(nums.slice(2));
-    
+    let exclude = nonAdjacentSum(nums, i + 1);
+    let include = nums[0] + nonAdjacentSum(nums, i + 2);
+    console.log([include, exclude])
     return Math.max(include, exclude);
 };
 
 
 
 const nums = [2, 4, 5, 12, 7];
+// const nums = [2, 4];
 // console.log(maxNeighborSum(4, nums))
 console.log(nonAdjacentSum(nums)); // -> 16
-memo = { '0': { '2': 7, '3': 14}}
+// memo = { '0': { '2': 7, '3': 14}}
