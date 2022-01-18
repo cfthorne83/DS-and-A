bd668fa4e8374d5b71e22e88f1adfa868a7864e7
cfthorne <cfthorne83@gmail.com>
2022-01-18 14:34:34 -0500

    add helper funcs

--- structy/dynamic_programming/summingSquares.js
+++ structy/dynamic_programming/summingSquares.js
@@ -1,3 +1,26 @@
-const summingSquares = (n) => {
-  // todo
-};
+// const summingSquares = (n) => {
+//     if (n === 0) return 0;
+//     const squares = getSquares(n);
+
+//     for (let square of squares){
+//         summingSquares(n - square)
+//     }
+//     // return 1 + 
+// };
+
+const getSquares = (n) => {
+    let squares = [];
+    for (let num = 1; num < n ; num++){
+        if (isSquare(num)) squares.push(num);
+    }
+    return squares;
+}
+
+const isSquare = (n) => {
+    for (let i = 1; i <= n; i++){
+        if (i * i === n) return true;
+    }
+    return false;
+}
+
+console.log(getSquares(12));
