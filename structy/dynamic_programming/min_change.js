33c00734b670dfa450c7d9fe4d51a25ed9f4f494
cfthorne <cfthorne83@gmail.com>
2022-01-19 12:07:58 -0500

    last refactor minChange; ;

--- structy/dynamic_programming/min_change.js
+++ structy/dynamic_programming/min_change.js
@@ -7,14 +7,14 @@
 const minChange = (amount, coins, min=Infinity, memo={}) => {
    if (amount in memo) return memo[amount];
    if (amount === 0) return 0;
-   if (amount < 0) return -1;
+   if (amount < 0) return Infinity;
 
    for (let coin of coins) {
        let count = 1 + minChange(amount - coin, coins, min, memo);
-       if (count < min && count > 0) min = count;
+       min = Math.min(count, min);
    }
    memo[amount] = min;
-   return memo[amount];
+   return memo[amount] === Infinity? -1 : memo[amount];
 };
 
 console.log(minChange(8, [1, 5, 4, 12])); // -> 2, because 4+4 is the minimum coins possible
