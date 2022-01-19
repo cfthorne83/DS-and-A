1ab0409866e6f5a409c57d6d4b8b24c02f5d4323
cfthorne <cfthorne83@gmail.com>
2022-01-19 12:54:03 -0500

    add countingChange

---
+++ dynamic_programming/countingChange.js
@@ -0,0 +1,9 @@
+// Write a function, countingChange, that takes in an amount and an array of 
+// coins. The function should return the number of different ways it is possible
+//  to make change for the given amount using the coins.
+
+// You may reuse a coin as many times as necessary.
+
+const countingChange = (amount, coins) => {
+  // todo
+};
