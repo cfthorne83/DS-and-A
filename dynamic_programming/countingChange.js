66a2f258de06b692c1e8ce66e5a61c7a5529e668
cfthorne <cfthorne83@gmail.com>
2022-01-19 13:05:49 -0500

    first attempt countingChange

--- dynamic_programming/countingChange.js
+++ dynamic_programming/countingChange.js
@@ -4,6 +4,14 @@
 
 // You may reuse a coin as many times as necessary.
 
-const countingChange = (amount, coins) => {
-  // todo
+const countingChange = (amount, coins, ways=0, memo={}) => {
+    if (amount === 0) return 1;
+    if (amount < 0) return 0;
+
+    for (let coin of coins){
+        ways += countingChange(amount - coin, coins, ways);
+    }
+    return ways;
 };
+
+console.log(countingChange(4, [1,2,3]))// -> 4
