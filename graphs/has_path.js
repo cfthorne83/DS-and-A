b2255330407d70738d292e698b76d617e9f0eb2c
cfthorne <cfthorne83@gmail.com>
2022-01-27 13:31:51 -0500

    add hasPath

---
+++ graphs/has_path.js
@@ -0,0 +1,19 @@
+// Write a function, hasPath, that takes in an object representing the adjacency 
+// list of a directed acyclic graph and two nodes (src, dst). The function should 
+// return a boolean indicating whether or not there exists a directed path between 
+// the source and destination nodes.
+
+const hasPath = graph => {
+    
+};
+
+const graph = {
+  f: ['g', 'i'],
+  g: ['h'],
+  h: [],
+  i: ['g', 'k'],
+  j: ['i'],
+  k: []
+};
+
+console.log(hasPath(graph, 'f', 'k')); // true
