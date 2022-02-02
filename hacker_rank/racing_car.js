03e413fb0ab9c2f3e4e3b3ff78003944c1b459c4
cfthorne <cfthorne83@gmail.com>
2022-02-02 15:47:40 -0500

    work on racing cars

--- hacker_rank/racing_car.js
+++ hacker_rank/racing_car.js
@@ -1,5 +1,16 @@
-const minMoves = (obstacles) => {
+const minMoves = (obstacles, currentLane=2, count=0) => {
     if (!obstacles.length) return 0;
+    if (obstacles[0] === currentLane) count++;
+    
+    let availLanes = [];
+    for (let i = 1; i <= 3; i++){
+        if (i !== obstacles[0]) availLanes.push(i);
+    }
+    
+    let right = minMoves(obstacles.slice(1), availLanes[0], count);
+    let left = minMoves(obstacles.slice(1), availLanes[1], count);
 
-    minMoves(obstacles.slice(1));
+    return count + Math.min(right, left);
 };
+let arr = [2, 1, 3, 2];
+console.log(minMoves(arr))
