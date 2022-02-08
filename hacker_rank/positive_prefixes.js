8368d294a544a92f9b151e781d12585652543fea
cfthorne <cfthorne83@gmail.com>
2022-02-08 12:21:50 -0500

    add positive prefixes

---
+++ hacker_rank/positive_prefixes.js
@@ -0,0 +1,11 @@
+function maxPosPrefixes(arr) {
+    // find every possible arrangement of integers
+    // calculate the psum of each
+    // return the num of pos integers of the arr with the most
+
+}
+
+arr = [-6, 3, 4, -10];
+//optimal arrangement = [3, 4, -6, -10]
+//psum = [3, 7, 1, -9]
+//return 3
