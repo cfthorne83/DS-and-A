19de87fb6d347cac57a70c4267d8fbd95168dbbe
cfthorne <cfthorne83@gmail.com>
2022-03-10 13:32:22 -0500

    refactor tribonacci

--- dynamic_programming/tribonnaci.js
+++ dynamic_programming/tribonnaci.js
@@ -1,10 +1,17 @@
+// Write a function tribonacci that takes in a number argument, n, and returns the 
+// n-th number of the Tribonacci sequence.
+// The 0-th and 1-st numbers of the sequence are both 0.
+// The 2-nd number of the sequence is 1.
+// To generate further numbers of the sequence, calculate the sum of previous 
+// three numbers.
+// Solve this recursively.
+
 const tribonacci = (n, memo={}) => {
     if (n in memo) return memo[n];
     if (n === 0 || n === 1) return 0;
     if (n === 2) return 1;
 
-    memo[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    let result = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    memo[n] = result;
     return memo[n];
 };
-
-console.log(tribonacci(5));
