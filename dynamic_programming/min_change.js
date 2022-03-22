ba7f6745ef79354aeb8f6e7a2dae793b2ea6ed97
cfthorne <cfthorne83@gmail.com>
2022-03-22 13:00:00 -0400

    r

--- dynamic_programming/min_change.js
+++ dynamic_programming/min_change.js
@@ -4,6 +4,23 @@
 
 // If it is not possible to create the amount, then return -1.
 
+//keep track of a min variable
+//iterate through the array and subtract coin from amt, keeping track of how 
+//many tree levels have been traversed = if amt is 0 and the levels are less 
+//than the min var, replace the min var
+//return the min var
+//if amount is not found return -1
+
+const minChange = (amt, coins, min=Infinity) => {
+    if (amt === 0) return min;
+    if (amt < 0) return -1;
+
+    for (let coin of coins){
+        let count = 1 + minChange(amt - coin, coins, min);
+        if (count < min && count > 0) min = count;
+    }
+    return min !== Infinity ? min : -1;
+};
 
 
 console.log(minChange(8, [1, 5, 4, 12])); // -> 2, because 4+4 is the minimum coins possible
