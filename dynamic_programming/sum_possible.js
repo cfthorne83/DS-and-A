0d3965da78df14e528667a474211915e4b3212ef
cfthorne <cfthorne83@gmail.com>
2022-03-22 12:05:29 -0400

    refactor sum_possible

--- dynamic_programming/sum_possible.js
+++ dynamic_programming/sum_possible.js
@@ -1,22 +1,31 @@
-const sumPossible = (amount, numbers, memo={}) => {
-    if ( amount in memo) return memo[amount];
-    if (amount === 0){
-        memo[amount] = true;
-        return memo[amount];
-    };
-    if (amount < 0){
-        memo[amount] = false;
-        return memo[amount];
-    };
+// Write a function sumPossible that takes in an amount and an array of positive 
+// numbers. The function should return a boolean indicating whether or not it is 
+// possible to create the amount by summing numbers of the array. You may reuse 
+// numbers of the array as many times as necessary.
 
-    for (let num of numbers) {
-        if (sumPossible(amount - num, numbers, memo) === true) return true; 
+// You may assume that the target amount is non-negative.
+
+//iterate through the numbers and subtract them from the amount, do that conse
+//cutively until you reach 0 or you reach a neg number
+//if 0 is reached, return true
+//if not return false
+
+const sumPossible = (amt, arr, hash={}) => {
+    if (amt in hash) return hash[amt];
+    if (amt === 0 ) return true;
+    if (amt < 0) return false;
+
+    for (let num of arr) {
+        if (sumPossible(amt - num, arr, hash)){
+            hash[amt] = true;
+            return true;
+        } 
     }
-
-    memo[amount] = false;
+    hash[amt] = false;
     return false;
-}
-// console.log(sumPossible(8, [5, 12, 4])); // -> true, 4 + 4
+};
+
+console.log(sumPossible(8, [5, 12, 4])); // -> true, 4 + 4
 
 
 
