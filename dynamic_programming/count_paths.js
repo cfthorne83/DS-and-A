7aee14b145b0d0cc2a4b2055e261e57dbd64c28e
cfthorne <cfthorne83@gmail.com>
2022-03-29 13:04:33 -0400

    redo count_paths

--- dynamic_programming/count_paths.js
+++ dynamic_programming/count_paths.js
@@ -4,22 +4,23 @@
 // the number of ways possible to travel from the top-left corner of the grid to 
 // the bottom-right corner.
 
-const countPaths = (grid, row=0, col=0, count=0, memo={}) => {
-    let pos = `${row},${col}`;
-    if (pos in memo) return memo[pos];
-  
-    if ( row >= grid.length) return 0;
-    if ( col >= grid[0].length) return 0;
-    if (grid[row][col] === "X") return 0;
-  
-    if (row === grid.length - 1 && col === grid[0].length - 1) return 1;
-    
+// start at [0,0], check all availble paths from that position and explore each 
+//path 
+//if pos = [grid.length, grid[0].length] return 1
 
-    let right = countPaths(grid, row + 1, col, memo);
-    let down = countPaths(grid, row, col + 1, memo);
+const countPaths = (grid, r=0, c=0, memo={}) => {
+  const pos = `${r},${c}`;
+  if(pos in memo) return memo[pos]; 
+  if (r >= grid.length || c >= grid[0].length) return 0;
+  if (grid[r][c] === "X") return 0;
+  if (r === grid.length - 1 && c === grid[0].length - 1) return 1;
 
-    memo[pos] = right + down;
-    return memo[pos];
+  let right = countPaths(grid, r + 1, c, memo);
+  let down = countPaths(grid, r, c + 1, memo);
+
+  memo[pos] = right + down;
+  console.log(memo);
+  return memo[pos];
 };
 
 const grid = [
