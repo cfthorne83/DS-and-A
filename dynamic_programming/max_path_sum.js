17b2b3aaed4e2c2d43e82c864bf6cec45d9d256e
cfthorne <cfthorne83@gmail.com>
2022-03-29 13:39:57 -0400

    redo max_path_sum

--- dynamic_programming/max_path_sum.js
+++ dynamic_programming/max_path_sum.js
@@ -5,17 +5,26 @@
 
 // You can assume that all numbers are non-negative.
 
+//move through the grid from [0,0] check right and down position
+//explore all possible paths and return the one with the largest sum 
+//if you are at the last position return the sum
+//if not add the current pos val to the sum 
+
+
+
 const maxPathSum = (grid, r=0, c=0, memo={}) => {
-    let pos = `${r},${c}`;
-    if (pos in memo) return memo[pos];
-    if (r === grid.length || c === grid[0].length) return 0;
-    if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
+  const pos = r + ',' + c;
+  if (pos in memo) return memo[pos];
+  if (r >= grid.length || c >= grid[0].length) return 0;
+  if (r === grid.length - 1 && c === grid[0].length - 1) return grid[r][c];
 
-    let down = maxPathSum(grid, r + 1, c, memo);
-    let right = maxPathSum(grid, r, c + 1, memo);
+  const right = maxPathSum(grid, r + 1, c, memo);
+  const down = maxPathSum(grid, r, c + 1, memo);
 
-    memo[pos] = grid[r][c] + Math.max(down, right);
-    return memo[pos];
+  const max = grid[r][c] + Math.max(right, down);
+  memo[pos] = max;
+  console.log(memo);
+  return memo[pos];
 };
 
 const grid = [
@@ -23,4 +32,8 @@ const grid = [
   [5, 1, 1],
   [3, 6, 1],
 ];
+// const grid = [
+//   [1, 9],
+//   [3, 6],
+// ];
 console.log(maxPathSum(grid)); // -> 18
