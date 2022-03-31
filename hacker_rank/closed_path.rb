b3451c4626215a7a0496add17aaff309ed4474db
cfthorne <cfthorne83@gmail.com>
2022-03-31 11:43:12 -0400

    finish closed_path ruby

--- hacker_rank/closed_path.rb
+++ hacker_rank/closed_path.rb
@@ -1,7 +1,21 @@
 #create a hash with numbers as keys and num of closed paths as vals
-#
+#iterate through the input num and keep track of a count var
+#increment the count with the val from the hash and return count
 
 def closed_path(num)
+    closed_paths = {0=> 1, 4=> 1, 6=> 1, 9=> 1, 8=> 2}
+    count = 0;
 
-
+    num.to_s.chars.each do |char|
+        int = char.to_i
+        # p int
+        if (closed_paths[int])
+            count += closed_paths[int]
+            # p closed_paths[int]
+        end
+    end
+    count
 end
+
+number = 1288
+p closed_path(number)
