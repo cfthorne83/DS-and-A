5bf990741325325bc3cae23f9a601d1aad281fce
cfthorne <cfthorne83@gmail.com>
2022-03-31 11:58:01 -0400

    add 3/28

---
+++ hacker_rank/3/28/compress_word.rb
@@ -0,0 +1,19 @@
+def compress_word(word, k)
+
+    #iterate over every char of the word
+    #if the char matches the last char: increment a consecutive lett var by one
+    #keep track of the first index of the first consecutive letter
+    #if consecutive letter var == k, slice the consecutive letters out of the word
+   
+    #continue to do so until there are no consecutive chars
+        #keep track of a dup boolean: set to true as default
+        #immediately set it to false when you enter the while loop
+        #set it to true when consecutive lett var reaches k
+        #keep looping until it is false
+    
+    
+
+
+end
+word = 'abbccb'
+p compress_word(word)
